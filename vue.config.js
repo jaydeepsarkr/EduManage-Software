2dd63a3383c06e935143a45e07a39e7c321944b4
Jaydeep Sarkar <jaydeepsarkr@gmail.com>
2025-07-13 15:22:51 +0530

    fixed the logo

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,36 @@ const webpack = require("webpack");
 
 module.exports = {
   transpileDependencies: true,
+
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
       }),
     ],
+  },
+
+  // 👇 Add this PWA section
+  pwa: {
+    name: "School Management System",
+    themeColor: "#2563EB",
+    backgroundColor: "#ffffff",
+    manifestOptions: {
+      short_name: "SchoolSys",
+      start_url: ".",
+      display: "standalone",
+      icons: [
+        {
+          src: "icons/192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "icons/512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
   },
 };
